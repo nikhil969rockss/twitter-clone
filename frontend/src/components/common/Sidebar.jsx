f27a4a0854126e8999bb5d5a4f7b0d14b3814f0f
nikhil969rockss <ns9013604867@gmail.com>
2025-07-17 21:04:43 +0530

    feat: logout function implement with tanstack query in sideBar

--- frontend/src/components/common/Sidebar.jsx
+++ frontend/src/components/common/Sidebar.jsx
@@ -5,8 +5,34 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const Sidebar = () => {
+    const {
+        mutate: logoutMutation,
+        isError,
+        isPending,
+        error,
+    } = useMutation({
+        mutationFn: async () => {
+            try {
+                const res = await fetch("/api/auth/logout", {
+                    method: "POST",
+                });
+
+                const data = await res.json();
+
+                if (!res.ok) throw new Error(data.error || "Failed to Logout");
+                return data;
+            } catch (error) {
+                throw new Error(error);
+            }
+        },
+        onSuccess: () => {
+            toast.success("Logout Successfully");
+        },
+    });
     const data = {
         fullName: "John Doe",
         username: "johndoe",
@@ -79,7 +105,13 @@ const Sidebar = () => {
                                     @{data?.username}
                                 </p>
                             </div>
-                            <BiLogOut className="w-5 h-5 cursor-pointer" />
+                            <BiLogOut
+                                className="w-5 h-5 cursor-pointer"
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    logoutMutation();
+                                }}
+                            />
                         </div>
                     </Link>
                 )}
